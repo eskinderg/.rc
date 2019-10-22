f80a889fd6bb2eb8f03a3bed44682129c62093b4
eskinderg <eskinderget@gmail.com>
2019-10-22 09:39:17 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,37 +1,79 @@
 #!/bin/bash
 
-ln -s ~/.rc/.vimrc ~/.vimrc
-ln -s ~/.rc/.bashrc ~/.bashrc
-ln -s ~/.rc/.tmux.conf ~/.tmux.conf
-ln -s ~/.rc/.zshrc ~/.zshrc
-ln -s ~/.rc/.profile ~/.profile
-ln -s ~/.rc/.xinitrc ~/.xinitrc
-ln -s ~/.rc/.Xresources ~/.Xresources # theme for xterm
-
-xrdb -merge ~/.Xresources             # theme for xterm
-
-if [ -d ~/.config/nvim ]              # Check if neovim is installed
+if [ ! -f ~/.vimrc ]
 then
-  ln -s ~/.rc/.vimrc ~/.config/nvim/init.vim
-  mkdir -p ~/.config/nvim/colors
-  ln -s ~/.rc/.themerc.vim ~/.config/nvim/colors/EskTheme.vim
+  ln -s $PWD/.vimrc ~/.vimrc
+fi
+
+if [ ! -f ~/.bashrc ]
+then
+  ln -s $PWD/.bashrc ~/.bashrc
+fi
+
+if [ ! -f ~/.tmux.conf ]
+then
+  ln -s $PWD/.tmux.conf ~/.tmux.conf
+fi
+
+if [ ! -f ~/.zshrc ]
+then
+  ln -s $PWD/.zshrc ~/.zshrc
+fi
+
+if [ ! -f ~/.profile ]
+then
+  ln -s $PWD/.profile ~/.profile
+fi
+
+if [ ! -f ~/.xinitrc ]
+then
+  ln -s $PWD/.xinitrc ~/.xinitrc
+fi
+
+if [ ! -f ~/.Xresources ]               # theme for xterm
+then
+  ln -s $PWD/.Xresources ~/.Xresources # theme for xterm
+  xrdb -merge ~/.Xresources             # theme for xterm
+fi
+
+if [ -d ~/.config/nvim ]                # Check if neovim is installed
+then
+  if [ ! -f ~/.config/nvim/init.vim ]
+  then
+    ln -s $PWD/.vimrc ~/.config/nvim/init.vim
+  fi
+
+  if [ ! -f ~/.config/nvim/colors/EskTheme.vim ]
+  then
+    mkdir -p ~/.config/nvim/colors
+    ln -s $PWD/.themerc.vim ~/.config/nvim/colors/EskTheme.vim
+  fi
 else
   echo "Neovim directory ~/.config/nvim does not exist"
 fi
 
 if [ -d ~/.vim ]                      # Check if vim is installed
 then
-  mkdir -p ~/.vim/colors
-  ln -s ~/.rc/.themerc.vim ~/.vim/colors/EskTheme.vim
+  if [ ! -f ~/.vim/colors/EskTheme.vim ]
+  then
+    mkdir -p ~/.vim/colors
+    ln -s $PWD/.themerc.vim ~/.vim/colors/EskTheme.vim
+  fi
 else
   echo "~/.vim directory does not exist"
 fi
 
 #i3status and i3status-rust config
-if [ -d ~/.config/i3 ]                      # Check if vim is installed
+if [ -d ~/.config/i3 ]
 then
-  ln -s ~/.rc/i3/config ~/.config/i3/config
-  ln -s ~/.rc/i3/i3status-rust/config.toml ~/.config/i3/i3status-rust/config.toml
+  if [ ! -f ~/.config/i3/config ]
+  then
+    ln -s $PWD/i3/config ~/.config/i3/config
+  fi
+  if [ ! -f ~/.config/i3/i3status-rust/config.toml ]
+  then
+    ln -s $PWD/i3/i3status-rust/config.toml ~/.config/i3/i3status-rust/config.toml
+  fi
 else
   echo "~/.config/i3 directory does not exist"
 fi
@@ -42,6 +84,13 @@ then
   then
     mv ~/.config/vifm/vifmrc ~/.config/vifm/vifmrc.backup
   fi
-  ln -s ~/.rc/vifmrc ~/.config/vifm/vifmrc
+  ln -s $PWD/vifmrc ~/.config/vifm/vifmrc
 fi
 
+GREEN="$(tput setaf 2)"
+printf '%s' "$GREEN"
+printf '%s\n'
+printf '===============================%s\n'
+printf '%s\n' 'Successful'
+printf '===============================%s\n'
+printf '%s\n'
