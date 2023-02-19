2e5bad436779e16f91b64e2cd31640f1ae48700b
Eskinder <eskinderget@gmail.com>
2023-02-19 17:44:03 +0300

    added tab key

--- .config/nvim-cmp.lua
+++ .config/nvim-cmp.lua
@@ -22,7 +22,21 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+      ['<S-Tab>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
