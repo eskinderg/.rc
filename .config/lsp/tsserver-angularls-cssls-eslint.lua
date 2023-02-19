61034d428e3d2ccae681d05cb46a739a32a2c62f
Eskinder <eskinderget@gmail.com>
2023-02-19 16:25:13 +0300

    plugins upgrade

--- .config/lsp/tsserver-angularls-cssls-eslint.lua
+++ .config/lsp/tsserver-angularls-cssls-eslint.lua
@@ -1,6 +1,6 @@
 nvim_lsp = require('lspconfig')
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local servers = { 'tsserver', 'angularls', 'cssls', 'eslint', 'html', 'vimls' }
 
