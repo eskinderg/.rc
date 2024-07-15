93eaba88c0e7fe14d18cc31cbde93dd244f46529
Eskinder <eskinderget@gmail.com>
2024-07-15 18:53:45 +0300

    changes

--- .config/treesitter.lua
+++ .config/treesitter.lua
@@ -45,7 +45,7 @@ require'nvim-treesitter.configs'.setup {
 
     -- list of language that will be disabled
     -- disable = { "c", "rust" },
-    disable = { "html", "typescript" },
+    -- disable = { "html", "typescript" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
