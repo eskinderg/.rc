ebd759d64b0b527cfdb2a67df5ee24637d1f0382
Eskinder <eskinderget@gmail.com>
2026-06-07 11:29:44 +0300

    gnome-settings

--- scripts/gnome-settings.sh
+++ scripts/gnome-settings.sh
@@ -6,7 +6,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 wget https://github.com/hardpixel/unite-shell/releases/download/v85/unite-v85.zip
 gnome-extensions install --force unite-v85.zip
 
-gnome-extensions install --force ../gnome-extensions/pcmonitor.zip
+gnome-extensions install --force $HOME/.rc/gnome-extensions/pcmonitor.zip
 
 gnome-extensions enable unite@hardpixel.eu
 gnome-extensions enable pcmonitor@eskinderget@gmail.com
